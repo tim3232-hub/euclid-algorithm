6681bd5175ff9a18d65c01976af4575aeb5e0cfb
tim3232-hub <64925258+tim3232-hub@users.noreply.github.com>
2023-02-04 16:50:07 +0100

    Add files via upload

---
+++ euclid_algorithm.js
@@ -0,0 +1,42 @@
+const Submit = document.querySelector('#Submit')
+function gcd(a, b) {
+    a = Number(integer_a.value);
+    b = Number(integer_b.value);
+    a1 = Number(integer_a.value);
+    b1 = Number(integer_b.value);
+    let r;
+    if (a % 1 !==0 || b % 1 !==0 || isNaN(a)) {
+        alert('a oder b ist keine ganze Zahl')
+        return false;
+    }
+    else if (a === b) {
+        document.getElementById("a1").innerHTML = a1;
+        document.getElementById("b1").innerHTML = b1;
+        document.getElementById("gcd").innerHTML = a;
+        return a;
+    }
+    else if (a === 0) {
+        document.getElementById("a1").innerHTML = a1;
+        document.getElementById("b1").innerHTML = b1;
+        document.getElementById("gcd").innerHTML = b;
+          return b;  
+    }
+    else if (b === 0) {
+        document.getElementById("a1").innerHTML = a1;
+        document.getElementById("b1").innerHTML = b1;
+        document.getElementById("gcd").innerHTML = a;
+        return a;
+    }
+    else {
+        while ((a % b) >0 ) {
+            r = a % b;
+            a = b;
+            b = r;
+        }
+        document.getElementById("a1").innerHTML = a1;
+        document.getElementById("b1").innerHTML = b1;
+        document.getElementById("gcd").innerHTML = b;
+        return b;
+    }  
+}
+Submit.addEventListener('click', gcd)
